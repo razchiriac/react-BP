01ed4ea52eb6bb80da0c526834765c6573db9c83
Raz <raz.chiriac@gmail.com>
2017-02-04 13:16:40 -0500

    initial commit
    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+var path = require('path');
+
+// allow express to serve webpack files
+var webpack = require('webpack');
+var config = require('./webpack.config');
+
+var express = require('express');
+var stormpath = require('express-stormpath');
+
+var app = express();
+
+// allow webpack to intercept requests and serve or packaged /js/app.js file
+var compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(stormpath.init(app, {
+  web: {
+    produces: ['application/json']
+  }
+}));
+
+app.get('/css/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/css/bootstrap.min.css'));
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+app.on('stormpath.ready', function () {
+  app.listen(3000, 'localhost', function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Listening at http://localhost:3000');
+  });
+});
